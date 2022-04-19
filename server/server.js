645bef604b9d73639b4eb8c42d3b28a6a28f00ca
owen <owen@pop-os.localdomain>
2022-04-18 18:01:03 -0700

    sqlite database

--- server/server.js
+++ server/server.js
@@ -1,16 +1,25 @@
 const express = require("express")
 const app = express()
+const path = require('path');
 
-const root = require('path').join(__dirname, '../client/build')
-app.use(express.static(root));
-app.get("*", (req, res) => {
-    res.sendFile('index.html', { root });
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./blogdb.sqlite');
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", function(req, res) {
+    db.serialize(function () {
+        db.all("SELECT * FROM posts", function(err, rows) {
+            console.log(err);
+            res.send(rows)
+        });
+    });
 })
 
 
-app.get("/", function(req, res) {
-    res.send("It's working!")
-})
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
   
 app.listen(3000, () => {
     console.log("app listening on port 3000")
